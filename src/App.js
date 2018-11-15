00e1c046d34316a88456068c99cba3a372fdbbd5
joshsarna <joshsarna@gmail.com>
2018-11-15 15:52:12 -0800

    pass props to Person component and render them

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Josh', age: 23},
+      {name: 'Maria', age: 23},
+      {name: 'Jake', age: 23}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person />
-        <Person />
-        <Person />
+        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }/>
+        <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }>My Hobbies: Hiking</Person>
+        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age }/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
