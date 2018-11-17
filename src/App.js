a0b80e1e1a0ee29c8b855908d0eaec6bd95eaaef
joshsarna <joshsarna@gmail.com>
2018-11-17 11:15:39 -0800

    refactor nameChnagedHandler so that only the element in the list that gets changed is re-rendered

--- src/App.js
+++ src/App.js
@@ -20,15 +20,21 @@ class App extends Component {
     this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        {name: 'Josh', age: 23},
-        {name: event.target.value, age: 23},
-        {name: 'Jake', age: 23}
-      ],
-      showPersons: false
-    } )
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} );
   }
 
   togglePersonsHandler = () => {
@@ -55,7 +61,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) =>this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       )
